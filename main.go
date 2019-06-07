1fb9d0e3ff1d94550c60920f07b5503309097069
Richard Eldridge <eldridge.rich@gmail.com>
2019-06-07 00:03:42 -0400

    play with Println

--- main.go
+++ main.go
@@ -6,5 +6,8 @@ import (
 
 func main() {
 	var str = "hello world"
-	fmt.Println(str)
+	// user _ to ignore one of the returns (either n or err, in this case)
+	n, err := fmt.Println(str, 41, true)
+	fmt.Println(n)
+	fmt.Println(err)
 }
