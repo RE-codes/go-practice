51b074a57d80faedfc6aa1713596b447137de45a
Richard Eldridge <eldridge.rich@gmail.com>
2019-06-14 23:11:44 -0400

    add prog-fundamentals folder

--- 001a - exercise/main.go
+++ 001a - exercise/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 // func main() {
@@ -15,11 +16,28 @@ import (
 // 	fmt.Println(z)
 // }
 
-var x int = 42
-var y string = "James Bond"
-var z bool = true
+// var x int = 42
+// var y string = "James Bond"
+// var z bool = true
+
+type banana int
+
+var y int
 
 func main() {
-	s := fmt.Sprintf("%v\t%v\t%v\t", x, y, z)
-	fmt.Println(s)
+	// s := fmt.Sprintf("%v\t%v\t%v\t", x, y, z)
+	// fmt.Println(s)
+	var x banana
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+	var temp = int(x)
+	fmt.Println("temp = ", temp)
+	fmt.Printf("temp is a %T\n", temp)
+	y = temp
+	fmt.Println("y = ", y)
+	fmt.Printf("y is of type %T\n", y)
+	fmt.Println(runtime.GOARCH)
+	fmt.Println(runtime.GOOS)
 }
---
+++ 002 - prog-fundamentals/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num := 384
+	fmt.Printf("%d\t%b\t%x\n", num, num, num)
+}
